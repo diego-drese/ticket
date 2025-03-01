bde027e981ab49b97b966e0df87a3b4f981769bc
Diego Neumann <diego.neumann.drese@gmail.com>
2025-03-01 15:07:57 -0300

    Adicionado arquivos hmtl dop larvel ao projeto

---
+++ resources/lang/pt_BR/notifications.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'reset_password_subject' => 'Redefinição de Senha',
+    'reset_password_line_1' => 'Você está recebendo este e-mail porque recebemos um pedido de redefinição de senha para sua conta.',
+    'reset_password_action' => 'Redefinir Senha',
+    'reset_password_line_2' => 'Se você não solicitou uma redefinição de senha, nenhuma ação adicional é necessária.',
+];
+
